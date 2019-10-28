a3be463a8172baaa10b924565902382b8e9a3b7e
Terry chan <napoler2008@gmail.com>
2019-10-29 06:44:23 +0800

    fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import find_packages, setup
 from os import path as os_path
 this_directory = os_path.abspath(os_path.dirname(__file__))
-
+import time
 # 读取文件内容
 def read_file(filename):
     with open(os_path.join(this_directory, filename), encoding='utf-8') as f:
@@ -16,16 +16,20 @@ def read_requirements(filename):
 
 setup(
     name='tseq2seq',
-    version='0.1.0.1',
+    version='0.1.0.1'+str(time.time()),
     description='tseq2seq',
     author='Terry Chan',
     author_email='napoler2008@gmail.com',
     url='https://github.com/napoler/tseq2seq',
-    install_requires=read_requirements('requirements.txt'),  # 指定需要安装的依赖
-    # install_requires=[
-    #     # 'beautifulsoup4==4.7.1',
-
-    # ],
+    # install_requires=read_requirements('requirements.txt'),  # 指定需要安装的依赖
+    install_requires=[
+        'tqdm==4.36.1',
+        'AutoBUlidVocabulary==0.1.0.11572302573.261918',
+        'numpy==1.17.3',
+        'requests==2.22.0',
+        'torch==1.3.0',
+        'torchvision==0.4.1',
+    ],
     packages=['src'])
 
 """
