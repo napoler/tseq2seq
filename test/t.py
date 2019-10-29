7834617b3b9db4c1d20a8dbdd12442a1ab241a55
Terry chan <napoler2008@gmail.com>
2019-10-30 02:24:27 +0800

    fix

--- test/t.py
+++ test/t.py
@@ -3,7 +3,10 @@ from __future__ import unicode_literals
 import sys
 sys.path.append("../")
 from tseq2seq import *
+# from tseq2seq.seq2seq import *
 from AutoBUlidVocabulary import *
+import random
+
 # Ts2s()
 # seq_data = [['b柯基犬是个子吗', '柯基犬'], ['哈士奇喜欢吃肉', '哈士奇'], ['猫咪喜欢吃鱼', '猫咪'], ['毛驴喜欢吃草', '毛驴'], ['牛逼的人', '人'], ['老牛拉车', '老牛'],['他将摘要抽取的指导准则设计为以下两个维度','摘要']]
 # ts2s=Ts2s(n_step = 30,n_hidden = 64,epoch=5,path='./')
@@ -51,18 +54,26 @@ def cmn_to_data():
     with open('cmn.txt', 'r', encoding='utf-8') as f:
         data = f.read()
     data = data.split('\n')
-    data = data[:100]
-    # print(data[-5:])
+    # data = data[:100]
+    # print(len(data))
+    data = data[:len(data)-1]
+    # data = random.sample(data, 50)  #从list中随机获取5个元素，作为一个片断返回 
+    # # print(data[-5:])
     # 分割英文数据和中文数据
     en_data = [line.split('\t')[0] for line in data]
     ch_data = ['' + line.split('\t')[1] + '' for line in data]
     new_en_data=[]
     for line in en_data:
         line = remove_symbols(line)
-        new_en_data.append(text_list(line))
+        new_en_data.append(line)
+        # new_en_data.append(text_list(line))
+        # print(line)
+        # new_en_data.append(line)
     new_ch_data=[]
     for line in ch_data:
-        new_ch_data.append(vocab.text_list(line))
+        it=' '.join(list(line))
+        # print(line)
+        new_ch_data.append(it)
 
     # print('英文数据:\n', new_en_data[:10])
     # print('\n中文数据:\n', new_ch_data[:5])
@@ -79,17 +90,43 @@ def cmn_to_data():
 
 
 
+
+# print(seq_data)
+
+# import numpy as np         
+#  import math
+# new=seq_data[:, ::2]
+
+# new=seq_data[:, ::2]
+# new=np.array(seq_data)
+# len(seq)
+
+
+# max_len =math.ceil(float(len(seq_data))/20)
+# min_len =math.ceil(len(seq_data)/20)
+# (max_len-min_len )*[]
+# new_seq_data=new.reshape((20,11))
+# print(new_seq_data)
+
+# print(new_seq_data.tolist())
+# exit()
+
+
 seq_data=cmn_to_data()
-Ts2s()
-# seq_data = [['b柯基犬是个子吗', '柯基犬'], ['哈士奇喜欢吃肉', '哈士奇'], ['猫咪喜欢吃鱼', '猫咪'], ['毛驴喜欢吃草', '毛驴'], ['牛逼的人', '人'], ['老牛拉车', '老牛'],['他将摘要抽取的指导准则设计为以下两个维度','摘要']]
-ts2s=Ts2s(n_step = 30,n_hidden = 64,epoch=500,path='./')
-# seq_data= rand_seq()
-ts2s.bulid_data(seq_data)
-ts2s.train()
-t=seq_data[:1]
 
+seq_data=seq_data[:10]
 
+for i in range(1):
+    Ts2s()
+    # seq_data = [['b柯基犬是个子吗', '柯基犬'], ['哈士奇喜欢吃肉', '哈士奇'], ['猫咪喜欢吃鱼', '猫咪'], ['毛驴喜欢吃草', '毛驴'], ['牛逼的人', '人'], ['老牛拉车', '老牛'],['他将摘要抽取的指导准则设计为以下两个维度','摘要']]
+    ts2s=Ts2s(n_step = 30,n_hidden = 32,epoch=2000,batch_size=20,path='./')
+    # seq_data= rand_seq()
+    # print(seq_data)
+    ts2s.bulid_data(seq_data)
+    ts2s.train()
+    t=seq_data[:5]
 
-# print('*'*50,n)
-print(t[0][1])
-print('生成标题 ->', ts2s.translate(t[0][0]))
+    for n in t:
+        print("原文: ",n[0])
+        print('正确翻译: ',n[1])
+        print('生成翻译 ->', ts2s.translate([n[0]]))
--- tseq2seq/ts2s.py
+++ tseq2seq/ts2s.py
@@ -8,8 +8,8 @@ from tqdm import tqdm
 import os
 
 from .seq2seq import *
-
-
+import numpy as np   
+import math
 
 dtype = torch.FloatTensor
 # S: Symbol that shows starting of decoding input
@@ -24,12 +24,20 @@ dtype = torch.FloatTensor
 
 
 class Ts2s:
-  def __init__(self,n_step = 3,n_hidden = 128,epoch=1000,path='./'):
+  def __init__(self,n_step = 3,n_hidden = 128,epoch=1000,batch_size=20,loss_num=10,path='./'):
     self.n_step = n_step  #文字长度
     self.n_hidden = n_hidden
-    self.epoch=epoch
+
     self.path=path+'model.pk'
     self.voc_path=path+''
+    #每次处理的数据条数
+    self.batch_size=batch_size
+    #输出loss的次数
+    self.loss_num=loss_num
+    #训练多少步
+    self.epoch=epoch
+
+
     # if torch.cuda.is_available():
     #   print("gpu")
     #   self.device='cuda'
@@ -46,8 +54,8 @@ class Ts2s:
     pass 
   def bulid_data(self,seq_data):
     """
-    构建数据
-    seq_data = [['b柯基犬是个狗子吗', '柯基犬'], ['哈士奇喜欢吃肉', '哈士奇'], ['猫咪喜欢吃鱼', '猫咪'], ['毛驴喜欢吃草', '毛驴'], ['牛逼的人', '人'], ['老牛拉车', '老牛']]
+    构建数据 字符用空格隔开
+    seq_data = [['b 柯 基 犬 是 个 狗 子 吗', '柯基犬'], ['哈士奇喜欢吃肉', '哈士奇'], ['猫咪喜欢吃鱼', '猫咪'], ['毛驴喜欢吃草', '毛驴'], ['牛逼的人', '人'], ['老牛拉车', '老牛']]
     """
     
     self.seq_data=seq_data
@@ -64,7 +72,7 @@ class Ts2s:
     # self.char_arr=list(self.num_dic)
     # # Seq2Seq Parameter
     # self.n_class = len(self.num_dic) #和字典一样 类似分类
-    self.batch_size = len(self.seq_data)
+    # self.batch_size = len(self.seq_data)
     #设置输入 x 的特征数量
     # self.batch_size = 30000
 
@@ -72,6 +80,7 @@ class Ts2s:
       """
       构建Tensor
       """
+      print('make_batch')
 
       input_batch, output_batch, target_batch = [], [], []
       # print('make_batch: ')
@@ -101,26 +110,69 @@ class Ts2s:
       return Variable(torch.Tensor(input_batch)).to(self.device), Variable(torch.Tensor(output_batch)).to(self.device), Variable(torch.LongTensor(target_batch)).to(self.device)
       # return Variable(torch.Tensor(input_batch)), Variable(torch.Tensor(output_batch)), Variable(torch.LongTensor(target_batch))
   def train(self):
-    #运行训练
-    input_batch, output_batch, target_batch = self.make_batch(self.seq_data)
-    ##执行加载模型 没有的话自动创建
+    # for self.seq_data
+
+    # max_len =math.ceil(float(len(self.seq_data))/20)
+    # # min_len =math.ceil(len(self.seq_data)/self.batch_size)
+    # ### 向上取整 用空余的数据补全
+    # new_len=max_len*self.batch_size
+    # pad_lines=[]
+    # for i in range(new_len-len(self.seq_data)):
+    #   pad_lines.append(['[PAD]','[PAD]'])
+    # self.new_seq_data=pad_lines+self.seq_data
+    # self.new_seq_data=np.array(self.new_seq_data)
+
+    # self.new_seq_data=self.new_seq_data.reshape((max_len,self.batch_size))
+    # # print(new_seq_data)
+
     self.load()
-    criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(self.model.parameters(), lr=0.001)
 
+    # print(self.new_seq_data.tolist())
+    if len(self.seq_data)>self.batch_size:
+      seq_batch=[]
+      for i,seq_one_batch in enumerate(self.seq_data):
+        #分批次执行
+        # seq_batch=[]
+
+        # i=0
+        if i%self.batch_size==0 and i!=0:
+          #如果整除批次的话
+
+          self.train_one_batch(seq_batch)
+          # print('整除',i)
+          # print('seq_batch',seq_batch)
+          seq_batch=[]
+        else:
+          # print("那奶奶",seq_one_batch)
+          seq_batch.append(seq_one_batch)
+ 
+        # print('22怒',i)
+      # 运行最后一次
+      self.train_one_batch(seq_batch)
+    else:
+      self.train_one_batch(self.seq_data)
+      pass
+          
+  def train_one_batch(self,seq_batch):
+    print('运行一个batch')
+    # print('self.batch_size',self.batch_size)
+    self.criterion = nn.CrossEntropyLoss()
+    self.optimizer = torch.optim.Adam(self.model.parameters(), lr=0.001)
+    self.lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(self.optimizer, T_max = (self.epoch // 9) + 1,eta_min=1e-10)
+        #运行训练
+    input_batch, output_batch, target_batch = self.make_batch(seq_batch)
+    ##执行加载模型 没有的话自动创建
     # lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max = (self.epoch // 10) + 1,eta_min=4e-08)
-
-    lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max = (self.epoch // 9) + 1,eta_min=1e-10)
- 
- 
-
     for epoch in tqdm(range(self.epoch)):
 
- 
-        # make hidden shape [num_layers * num_directions, batch_size, n_hidden]
-        hidden = Variable(torch.zeros(1, self.batch_size, self.n_hidden)).to(self.device)
+        # self.batch_size = len(self.seq_data)
 
-        optimizer.zero_grad()
+        # make hidden shape [num_layers * num_directions, batch_size, n_hidden]
+        # hidden = Variable(torch.zeros(1, self.batch_size, self.n_hidden)).to(self.device)
+        #  full_size = len(self.seq_data)
+
+        hidden = Variable(torch.zeros(1, len(seq_batch), self.n_hidden)).to(self.device)
+        self.optimizer.zero_grad()
         # input_batch : [batch_size, max_len(=n_step, time step), n_class]
         # output_batch : [batch_size, max_len+1(=n_step, time step) (becase of 'S' or 'E'), n_class]
         # target_batch : [batch_size, max_len+1(=n_step, time step)], not one-hot
@@ -132,17 +184,19 @@ class Ts2s:
         acc=0
         for i in range(0, len(target_batch)):
             # output[i] : [max_len+1, num_directions(=1) * n_hidden, target_batch[i] : max_len+1]
-            loss += criterion(output[i], target_batch[i])
+            loss += self.criterion(output[i], target_batch[i])
             acc_one=self.compute_loss(output[i], target_batch[i])
             acc+=acc_one
-        if (epoch + 1) % 2 == 0:
-            print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.6f}'.format(loss))
+        # loss_num =math.ceil(float(len(self.epoch))/10)
+        if (epoch + 1) % self.loss_num  == 0:
+            print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.6f}'.format(loss),'acc =', '{:.6f}'.format(acc))
             # print('Epoch:', '%04d' % (epoch + 1), 'acc =', '{:.6f}'.format(acc))
         loss.backward()
         # print(loss)
-        lr_scheduler.step(acc)
- 
-        optimizer.step()
+
+
+        self.optimizer.step()
+        self.lr_scheduler.step(acc)
         torch.save(self.model.state_dict(), self.path)
 
   def compute_loss(self,predicts , labels  ):
@@ -174,23 +228,33 @@ class Ts2s:
 
     # self.batch_size = len(self.seq_data)
   # Test
-  def translate(self,word):
+  def translate(self,seq_list):
       """
       执行预测
+      多个预测 使用空格分割
+      seq_list=['你 好','怎 么 样']
       """
       self.load()
       # input_batch, output_batch, _ = self.make_batch([[word, '[PAD]' * len(word)]])
-      input_batch, output_batch, _ = self.make_batch([[word, '' ]])
+      # input_batch, output_batch, _ = self.make_batch([[word, '' ]])
+
+      input_batch, output_batch, _ = self.make_batch(seq_list)
       # make hidden shape [num_layers * num_directions, batch_size, n_hidden]
       hidden = Variable(torch.zeros(1, 1, self.n_hidden)).to(self.device)
       #加载模型
 
       output = self.model(input_batch, hidden, output_batch)
       # output : [max_len+1(=6), batch_size(=1), n_class]
+      # print("output.data.max(2, keepdim=True)",output.data.max(2, keepdim=True))
+      # predict0 = output.data.max(2, keepdim=True)[0] # select n_class dimension
+      # decoded0 = [self.char_arr[i] for i in predict0]
+      # print(decoded0)
+
+
 
       predict = output.data.max(2, keepdim=True)[1] # select n_class dimension
       decoded = [self.char_arr[i] for i in predict]
-      # print(decoded)
+      print(decoded)
       try:
         end = decoded.index('[SEP]')
         translated = ''.join(decoded[:end])
